ff70a3b7c5fddfcd11912078cee66af58db49348
gonzalo stampa <gonzalo.stampa@alumnos.upm.es>
2022-05-03 02:19:33 +0200

    boradas anotaciones innecesaruias
    en la anterior actualizacion se me olvido borrar algunas anotaciones innecesarias

--- main.c
+++ main.c
@@ -2,21 +2,12 @@
 #include"adddelay.h"
 //#include<wchar.h>
 int main(){
-
 //indicamos al usuario las instrucciones a seguir para comletar el primer escenario/nivel/mapa dando tiempo entre indicaciones
 //haciendo uso de la funcion delay
 //imprimimos 10000 saltos de linea ya para llegar al final de el ejecutador del programa ya que asi las imagenes se ven mas fluidas
 for(int i=0;i<10000;i++)printf("\n");
 casa();
 oscuridad1();
-/*
-delay(3);
-printfdelay("... al pisar ves que el suelo resbala tanto que para frenarte tienes que chocar con un obstaculo o una pared ...\n\n" , 3);
-printfdelay("para moverte usa las teclas 'W','A','S','D' \n\n" , 3);
-printfdelay("tu objetivo es llegar hasta la salida, en la parte superior derecha, para pasar a la siguiente sala\n\n" , 3);
-printf("pulsa cualquier tecla para empezar");
-getch();
-*/
 delay(1);
 printf("\n\n\n\n");
 //graficamos el mapa en la posicion inicial(1 , 0) y con la funcion posicion calcularemos en que posicion se encuentra en cada instante el usuario y la graficaremos
